9e4282e979ee4b1c9c26b3750f9e05743b1beeb9
Patrick Wicki <patrick.wicki96@gmail.com>
2024-07-26 18:19:54 +0200

    plot.py: output info header for latex plots

--- pan/config.py
+++ pan/config.py
@@ -2,7 +2,7 @@
 
 # Defaults variants for which we generate scenarios
 DEFAULT_FLOW_NUMBERS = [4, 8, 12, 16, 20, 24]
-DEFAULT_SCENARIO_TYPES = ['Ciao', 'Naive', 'BGP FD', 'BGP ECMP']
+DEFAULT_SCENARIO_TYPES = ['Ciao', 'Naive', 'BGP_FD', 'BGP_ECMP']
 
 # Time at which target flows start sending data
 STARTUP_PHASE_SECONDS = 1800
